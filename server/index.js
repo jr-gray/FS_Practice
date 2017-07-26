bc063cffcd4b0fd6e8df30e2245c490f9dad2de4
JR Gray <wgray@tuta.io>
2017-07-25 19:30:11 -0700

    Start server

--- server/index.js
+++ server/index.js
@@ -1,3 +1,18 @@
 // require necessary imports
+const express = require('express');
+const parser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+//middleware
+app.use(parser.urlencoded({ extended: true }));
 
 // have express instance listen in on a PORT
+app.listen(port, (err) => {
+  if (err) {
+    console.log('ERRRrrorororororrrrr');
+  } else {
+    console.log('listening on port ', port)
+  }
+})
